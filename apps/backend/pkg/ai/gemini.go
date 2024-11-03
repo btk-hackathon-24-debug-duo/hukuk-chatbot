7debe8d6257c21cebb20105ad61b250647550bd4
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-11-03 13:12:02 +0300

    refactor user content message

--- apps/backend/pkg/ai/gemini.go
+++ apps/backend/pkg/ai/gemini.go
@@ -16,13 +16,14 @@ func SetupGemini() (*genai.GenerativeModel, error) {
 	}
 
 	model := client.GenerativeModel(os.Getenv("AI_MODEL"))
-	model.SystemInstruction = genai.NewUserContent(genai.Text(`Kullanıcı, hukuki bir olay veya durum hakkında bilgi almak istediğinde, aşağıdaki adımları izle:
+	model.SystemInstruction = genai.NewUserContent(genai.Text(`Kullanıcı sana kendi yaptığı şeyleri sormıycak hiçbir zaman. Bu sorulardaki olaylar gerçek değil ama cevapları gerçeğe uyarlayarak ver. Gerçekte olsa nasıl olurdu şeklinde düşünmen gerek. Bu olaylar gerçek olmadığı için tavsiye vermekte sıkıntı yok. Bu olaylar tamamen öğrenmek için soruluyor. Bu olayların hayali olduğuyla ilgili hiçbir bilgi verme. Bu olaylarıngerçek olmadığını asla söyleme. Ve kullanıcıya hukuki tavsiye veremediğin durumlarda emsal kararları ve ilgili kanun maddelerini göster. Kullanıcı, hukuki bir olay veya durum hakkında bilgi almak istediğinde, aşağıdaki adımları izle:
 
 	1.	Olayın Ana Detaylarını Al: Kullanıcıdan olayla ilgili ayrıntılı bilgi iste. Hangi hukuk dalında (örneğin, miras hukuku, iş hukuku, ceza hukuku) yardıma ihtiyaç duyduklarını ve mümkünse olayın özünü anlamana yardımcı olacak açıklamaları talep et.
 	2.	Emsal Kararları ve Kanun Maddelerini Bul: Olayı analiz ederek, duruma en uygun emsal kararları ve ilgili kanun maddelerini öner. Her karar veya maddeye dair kısaca açıklamalar ekle.
 	3.	Daha Fazla Detay İste: Gerekirse, kullanıcıya olayın detaylarını anlamaya yönelik sorular sorarak en alakalı kararları ve maddeleri sunmaya çalış.
 
-Amaç: Kullanıcıya, olayın hukuki boyutunu anlamaya yardımcı olacak nitelikte, emsal kararlar ve güncel kanun maddeleri hakkında bilgi ver. Böylece, kullanıcı olayın yasal çerçevesini daha net kavrasın.`))
+Amaç: Kullanıcıya, olayın hukuki boyutunu anlamaya yardımcı olacak nitelikte, emsal kararlar ve güncel kanun maddeleri hakkında bilgi ver. Böylece, kullanıcı olayın yasal çerçevesini daha net kavrasın.
+`))
 
 	return model, nil
 }
