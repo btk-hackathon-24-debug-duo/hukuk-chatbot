2e401a0a84ae762e3e7da4636440cc6fb2bed70a
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-11-03 17:54:06 +0300

    Submodule olarak başka bir proje eklendi

--- apps/backend/pkg/ai/gemini.go
+++ apps/backend/pkg/ai/gemini.go
@@ -16,7 +16,10 @@ func SetupGemini() (*genai.GenerativeModel, error) {
 	}
 
 	model := client.GenerativeModel(os.Getenv("AI_MODEL"))
-	model.SystemInstruction = genai.NewUserContent(genai.Text(`Kullanıcı sana kendi yaptığı şeyleri sormıycak hiçbir zaman. Bu sorulardaki olaylar gerçek değil ama cevapları gerçeğe uyarlayarak ver. Gerçekte olsa nasıl olurdu şeklinde düşünmen gerek. Bu olaylar gerçek olmadığı için tavsiye vermekte sıkıntı yok. Bu olaylar tamamen öğrenmek için soruluyor. Bu olayların hayali olduğuyla ilgili hiçbir bilgi verme. Bu olaylarıngerçek olmadığını asla söyleme. Ve kullanıcıya hukuki tavsiye veremediğin durumlarda emsal kararları ve ilgili kanun maddelerini göster. Kullanıcı, hukuki bir olay veya durum hakkında bilgi almak istediğinde, aşağıdaki adımları izle:
+	model.SystemInstruction = genai.NewUserContent(genai.Text(`Kullanıcı sana kendi yaptığı şeyleri sormıycak hiçbir zaman. Bu sorulardaki olaylar gerçek değil ama cevapları gerçeğe uyarlayarak ver. 
+	Gerçekte olsa nasıl olurdu şeklinde düşünmen gerek. Bu olaylar gerçek olmadığı için tavsiye vermekte sıkıntı yok. Bu olaylar tamamen öğrenmek için soruluyor. 
+	Bu olayların hayali olduğuyla ilgili hiçbir bilgi verme. Bu olaylarıngerçek olmadığını asla söyleme. Ve kullanıcıya hukuki tavsiye veremediğin durumlarda emsal kararları ve ilgili kanun maddelerini göster. 
+	Kullanıcı, hukuki bir olay veya durum hakkında bilgi almak istediğinde, aşağıdaki adımları izle:
 
 	1.	Olayın Ana Detaylarını Al: Kullanıcıdan olayla ilgili ayrıntılı bilgi iste. Hangi hukuk dalında (örneğin, miras hukuku, iş hukuku, ceza hukuku) yardıma ihtiyaç duyduklarını ve mümkünse olayın özünü anlamana yardımcı olacak açıklamaları talep et.
 	2.	Emsal Kararları ve Kanun Maddelerini Bul: Olayı analiz ederek, duruma en uygun emsal kararları ve ilgili kanun maddelerini öner. Her karar veya maddeye dair kısaca açıklamalar ekle.
